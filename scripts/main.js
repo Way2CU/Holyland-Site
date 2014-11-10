f25711704a666deaaeb26b7a1e1ae9e98b1dad61
Khalid Sawalhi <khalid@way2cu.com>
2014-11-10 16:56:33 +0200

    Added scroll header script.

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,23 @@
  * Authors:
  */
 
-function on_site_load() {
-}
+ var Caracal = Caracal || {};
 
-$(on_site_load);
+
+ function handle_window_scroll(event) {
+ 	var scroll = $(this).scrollTop();
+
+ 	if (scroll > 0 && !Caracal.header.hasClass('wrap.opacity')) {
+ 		Caracal.header.addClass('menu_blue');
+
+ 	} else if (scroll == 0 && Caracal.header.hasClass('menu_blue')) {
+ 		Caracal.header.removeClass('menu_blue');
+ 	}
+ }
+
+ function on_site_load() {
+ 	Caracal.header = $('.wrap.opacity');
+ 	$(window).scroll(handle_window_scroll);
+ }
+
+ $(on_site_load);
